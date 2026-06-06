6d141d1747523c662f04a95aa020f5b9bd3ed30a
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-06-05 21:29:36 -0700

    Use latest lighter about group image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     url: "/about",
     images: [
       {
-        url: "/founder-margaret-family-watercolor-light.png",
+        url: "/founder-margaret-family-watercolor-lighter.png",
         width: 2192,
         height: 1982,
         alt: "DailyCall founder Matt Hemsing with his grandmother Margaret and family",
@@ -106,7 +106,7 @@ export default function AboutPage() {
         </div>
 
         <Image
-          src="/founder-margaret-family-watercolor-light.png"
+          src="/founder-margaret-family-watercolor-lighter.png"
           alt="Matt Hemsing with his grandmother Margaret and family"
           width={2192}
           height={1982}
